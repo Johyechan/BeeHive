44c226b787d8da500287a8c195093a3d3ce21f46
Johyechan <johyechan6@gmail.com>
2025-11-10 18:14:39 +0900

    Developing Game System
    사운드 추가 및 사운드 매니저 개발 중

---
+++ BeeHive/Assets/02_Scripts/InGame/MyManager/SoundManager.cs
@@ -0,0 +1,13 @@
+using MyUtil;
+using UnityEngine;
+
+namespace InGame.MyManager
+{
+    // 작성자: 조혜찬
+    // 사운드 매니저
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        
+    }
+}
+// 마지막 작성 일자: 2025.11.10
