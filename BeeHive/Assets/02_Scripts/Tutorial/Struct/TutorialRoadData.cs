128d709ad746cd64f12f1fa43da67bd0ed2aa631
Johyechan <johyechan6@gmail.com>
2026-03-06 17:47:52 +0900

    Developing Tutorial
    튜토리얼 개발 중
     - 처음 배치되어 있는 상태를 인식하는 초기화 기능 개발

---
+++ BeeHive/Assets/02_Scripts/Tutorial/Struct/TutorialRoadData.cs
@@ -0,0 +1,15 @@
+using InGame.MyObject.Piece;
+using System;
+
+namespace Tutorial.Struct
+{
+    // 작성자: 조혜찬
+    // 튜토리얼을 위해 세팅되어 있는 객체들의 연결을 위한 도로 구조체
+    [Serializable] // Inspector 창에서 값을 할당하기 위한 직렬화
+    public struct TutorialRoadData
+    {
+        public PieceBase road; // 도로
+        public int connectNumber; // 연결 번호
+    }
+}
+// 마지막 작성 일자: 2026.03.06
---
+++ BeeHive/Assets/02_Scripts/Tutorial/TutorialSetting.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using Tutorial.Struct;
+using UnityEngine;
+
+namespace Tutorial
+{
+    // 작성자: 조혜찬
+    // 튜토리얼 세팅 클래스
+    public class TutorialSetting : MonoBehaviour
+    {
+        [SerializeField] private List<TutorialRoadPlacePlaneData> _roadPlacePlanes;
+        [SerializeField] private List<TutorialRoadData> _roads;
+        [SerializeField] private List<TutorialPiecePlacePlaneData> _piecePlacePlanes;
+        [SerializeField] private List<TutorialPieceData> _pieces;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        // 초기화 함수
+        private void Init()
+        {
+            foreach(var roadPlacePlane in  _roadPlacePlanes)
+            {
+                foreach(var road in _roads)
+                {
+                    if(roadPlacePlane.connectNumber == road.connectNumber) // 연결 번호가 일치한다면
+                    {
+                        roadPlacePlane.roadPlacePlane.PlacedObjectType = road.road.CurrentObjectType; // 배치된 객체 타입 저장
+                        roadPlacePlane.roadPlacePlane.TeamType = road.road.CurrentTeamType; // 배치된 객체 팀 저장
+                        roadPlacePlane.roadPlacePlane.PlacedPiece = road.road; // 배치된 객체 저장
+
+                        road.road.PieceVariable.currentRoadPlacePlane = roadPlacePlane.roadPlacePlane; // 배치 칸 저장
+                    }
+                }
+            }
+
+            foreach(var piecePlacePlane in _piecePlacePlanes)
+            {
+                foreach(var piece in _pieces)
+                {
+                    if(piecePlacePlane.connectNumber == piece.connectNumber) // 연결 번호가 일치한다면
+                    {
+                        piecePlacePlane.piecePlacePlane.PlacedObjectType = piece.piece.CurrentObjectType; // 배치된 객체 타입 저장
+                        piecePlacePlane.piecePlacePlane.TeamType = piece.piece.CurrentTeamType; // 배치된 객체 팀 저장
+                        piecePlacePlane.piecePlacePlane.PlacedPiece = piece.piece; // 배치된 객체 저장
+
+                        piece.piece.PieceVariable.currentPlacePlane = piecePlacePlane.piecePlacePlane; // 배치 칸 저장
+                    }
+                }
+            }
+        }
+    }
+}
+// 마지막 작성 일자: 2026.03.06
